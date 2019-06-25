c6cd284e3b6c709107dc02c283a63ef17f995009
Hanna Baber <Hanna.Baber@itechart-group.com>
2019-06-25 09:23:34 +0300

    new function

--- NodejsConsoleApp3/app.js
+++ NodejsConsoleApp3/app.js
@@ -1,16 +1,23 @@
-'use strict';
-function once(fn, context) {
+function spyOn(func) {
+    var spy;
+    var count = 0;
+    var inputData = [];
     var result;
-    return function () {
-        if (fn) {
-            this.result = fn.apply(context || this, arguments);
-            fn = null;
-            return this.result;
-        }
-        return undefined;
+    var outputData = [];
+    spy = function () {
+        count++;
+        inputData.push(arguments);
+        result = func.apply(this, arguments);
 
-    };
-}
-logOnce = once(console.log)
-logOnce("foo") // -> "foo"
-logOnce("bar") // -> no effect
+        outputData.push(result);
+        return result;
+    }
+    spy.callCount = function () {
+        return count;
+    }
+    spy.wasCalledWith = function (value) {
+        return inputData.indexOf(value) === -1 ? false : true;
+    }
+    spy.returned = function (value) {
+        return outputData.indexOf(value) === -1 ? false : true;
+    }
